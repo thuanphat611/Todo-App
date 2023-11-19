cac70b37d57e29480da5104bbb3e178bb2e8b180
Thuận Phát <phat06112003@gmail.com>
2023-11-19 23:03:45 +0700

    add open/close modal function

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("hello world!");
+const addBtn = document.querySelector('.add-button');
+const closeModalBtn = document.querySelector('.close-modal');
+const myModal = document.querySelector('.modal');
+const addForm = document.querySelector('.save-btn');
+console.log(addForm);
+const toggleModal = () => {
+    myModal === null || myModal === void 0 ? void 0 : myModal.classList.toggle("no-display");
+};
+addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", () => {
+    toggleModal();
+});
+closeModalBtn === null || closeModalBtn === void 0 ? void 0 : closeModalBtn.addEventListener("click", () => {
+    toggleModal();
+});
+addForm === null || addForm === void 0 ? void 0 : addForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleModal();
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,23 @@
-console.log("hello world!");
+const addBtn = document.querySelector('.add-button');
+const closeModalBtn = document.querySelector('.close-modal');
+const myModal = document.querySelector<HTMLDivElement>('.modal');
+const addForm = document.querySelector('.save-btn');
+
+console.log(addForm);
+const toggleModal = (): void => {
+    myModal?.classList.toggle("no-display");
+};
+
+addBtn?.addEventListener("click", () => {
+  toggleModal();
+});
+
+closeModalBtn?.addEventListener("click", () => {
+  toggleModal();
+});
+
+addForm?.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  toggleModal();
+});
