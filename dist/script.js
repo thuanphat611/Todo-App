50081119060faa50936e9803c2fd537be7ea316c
Thuận Phát <phat06112003@gmail.com>
2023-11-20 12:48:10 +0700

    add change tab animation

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,15 @@
 "use strict";
+const currentTaskNav = document.querySelectorAll('.nav-item')[0];
+const completedTaskNav = document.querySelectorAll('.nav-item')[1];
 const addBtn = document.querySelector('.add-button');
 const closeModalBtn = document.querySelector('.close-modal');
 const myModal = document.querySelector('.modal');
-const addForm = document.querySelector('.save-btn');
-console.log(addForm);
+const addTaskBtn = document.querySelector('.save-btn');
+const changeTab = () => {
+    console.log('change');
+    currentTaskNav.classList.toggle('active');
+    completedTaskNav.classList.toggle('active');
+};
 const toggleModal = () => {
     myModal === null || myModal === void 0 ? void 0 : myModal.classList.toggle("no-display");
 };
@@ -13,7 +19,9 @@ addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click",
 closeModalBtn === null || closeModalBtn === void 0 ? void 0 : closeModalBtn.addEventListener("click", () => {
     toggleModal();
 });
-addForm === null || addForm === void 0 ? void 0 : addForm.addEventListener('click', (e) => {
+addTaskBtn === null || addTaskBtn === void 0 ? void 0 : addTaskBtn.addEventListener('click', (e) => {
     e.preventDefault();
     toggleModal();
 });
+currentTaskNav === null || currentTaskNav === void 0 ? void 0 : currentTaskNav.addEventListener('click', () => { changeTab(); });
+completedTaskNav === null || completedTaskNav === void 0 ? void 0 : completedTaskNav.addEventListener('click', () => { changeTab(); });
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,18 @@
+const currentTaskNav = document.querySelectorAll('.nav-item')[0];
+const completedTaskNav = document.querySelectorAll('.nav-item')[1];
+
 const addBtn = document.querySelector('.add-button');
 const closeModalBtn = document.querySelector('.close-modal');
 const myModal = document.querySelector<HTMLDivElement>('.modal');
-const addForm = document.querySelector('.save-btn');
+const addTaskBtn = document.querySelector('.save-btn');
 
-console.log(addForm);
+
+const changeTab = (): void => {
+  console.log('change');
+  currentTaskNav.classList.toggle('active');
+  completedTaskNav.classList.toggle('active');
+};
+
 const toggleModal = (): void => {
     myModal?.classList.toggle("no-display");
 };
@@ -16,8 +25,13 @@ closeModalBtn?.addEventListener("click", () => {
   toggleModal();
 });
 
-addForm?.addEventListener('click', (e) => {
+addTaskBtn?.addEventListener('click', (e) => {
   e.preventDefault();
-  
+
+  //add task
+
   toggleModal();
 });
+
+currentTaskNav?.addEventListener('click', () => { changeTab() });
+completedTaskNav?.addEventListener('click', () => { changeTab() });
